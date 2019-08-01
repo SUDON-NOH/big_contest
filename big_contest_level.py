d2226642598f8e8209e40f03cf57a21d9863e5e6
DONY NOH <51067105+SUDON-NOH@users.noreply.github.com>
2019-08-01 21:17:58 +0900

    Update big_contest_level.py

--- big_contest_level.py
+++ big_contest_level.py
@@ -1,11 +1,17 @@
 import pandas as pd
-
-
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib
+matplotlib.rcParams['font.family']='Malgun Gothic'   # 한글 사용
+matplotlib.rcParams['axes.unicode_minus'] = False
 
 activity = pd.read_csv('train_activity.csv')
 label = pd.read_csv('train_label.csv')
 combat = pd.read_csv('train_combat.csv')
 
+# Level 별 이탈 현황 그래프 만들기
+label.loc[label["survival_time"] == 64 , "survived"] = 1
+label.loc[label["survival_time"] < 64 , "survived"] = 0
 
 # activity와 label을 우선적으로 merge
 meg_act_lab = pd.merge(label, activity, how = 'inner', on = 'acc_id')
@@ -18,6 +24,7 @@ print(combat.columns)
 activity_group = activity.groupby('char_id').sum()[['playtime', 'npc_kill', 'solo_exp', 'party_exp',
                                                   'quest_exp','boss_monster', 'death', 'revive', 'exp_recovery',
                                                   'fishing','private_shop', 'game_money_change', 'enchant_count']]
+print(activity_group.count())
 
 # char_id 기준으로 groupby.sum() / combat에서 사용할 columns 추출
 combat_group = combat.groupby('char_id').sum()[['pledge_cnt','random_attacker_cnt', 'random_defender_cnt', 'temp_cnt',
@@ -25,10 +32,10 @@ combat_group = combat.groupby('char_id').sum()[['pledge_cnt','random_attacker_cn
 
 # char_id 기준 level 추출
 combat_lv = combat.groupby('char_id').max()['level']
-
+print(combat_lv.count())
 
 # char_id 를 label 에 추가하는 과정
-selected_group = meg_act_lab[['acc_id', 'char_id', 'survival_time', 'amount_spent']]
+selected_group = meg_act_lab[['acc_id', 'char_id', 'survival_time', 'amount_spent', 'survived']]
 
 # char_id를 기준으로 level 과 acc_id 를 추가하는 과정
 # ( activity_group에 직접 추가를 해도 되지만, acc_id와 survival_time 이 필요하기 때문에 과정을 한번 더 거친다.)
@@ -68,4 +75,12 @@ combat_group1 = pd.merge(combat_group, selected, how = 'inner', on = 'char_id')
 corr_level_active = activity_group3.corr()
 corr_level_combat = combat_group1.corr()
 
-# Level 별 이탈 현황 그래프 만들기
+# 그래프
+sns.countplot(data = activity_group3, x="level", hue="survived")
+plt.grid(True)
+plt.xlabel('level 범주')
+plt.ylabel('Survival count')
+plt.title('생존/비생존 숫자')
+plt.show()
+
+print(activity_group3.count())
